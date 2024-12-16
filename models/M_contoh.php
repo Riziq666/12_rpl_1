2a75756d491ea8a724669fc80f2931fc12fdf847
Riziq666 <mhmdriziq0807>
2024-12-16 09:02:20 +0700

    commit pertama

---
+++ models/M_contoh.php
@@ -0,0 +1,20 @@
+<?php
+
+class M_contoh
+
+{
+    // bikin 3 fungsi, tampil, edit, hapus
+}
+public function tampil() {
+        // fungsi untuk menampilkan data dari database
+}
+
+    function edit() {
+        // fungsi untuk edit data dari database
+    }
+
+    function edit() {
+        // fungsi untuk edit data dari database
+    }
+
+    ?>
